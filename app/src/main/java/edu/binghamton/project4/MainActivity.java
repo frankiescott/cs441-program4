5c49fbdb0e58408da8e148d16da0819a2a905153
Frankie <email@frankiescott.net>
2019-06-30 19:26:24 -0400

    set up example code

--- app/src/main/java/edu/binghamton/project4/MainActivity.java
+++ app/src/main/java/edu/binghamton/project4/MainActivity.java
@@ -2,12 +2,37 @@ package edu.binghamton.project4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RecyclerAdapter.ItemClickListener {
+
+    RecyclerAdapter adapter;
+    TextView topline;
+    ArrayList<String> entries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
     }
 }
