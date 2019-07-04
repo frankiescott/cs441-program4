e883b127574b10a648bb1592b3fe01a1bcb5aa56
Frankie <email@frankiescott.net>
2019-07-04 16:57:25 -0400

    started code for seekbars

--- app/src/main/java/edu/binghamton/project4/Main2Activity.java
+++ app/src/main/java/edu/binghamton/project4/Main2Activity.java
@@ -2,12 +2,60 @@ package edu.binghamton.project4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private int m;
+    private int b;
+    SeekBar sbm;
+    SeekBar sbb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        m = 1;
+        b = 1;
+        sbm = findViewById(R.id.sbm);
+        sbb = findViewById(R.id.sbb);
+
+        configureSeekBars();
+    }
+
+    public void configureSeekBars() {
+        sbm.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        sbb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
