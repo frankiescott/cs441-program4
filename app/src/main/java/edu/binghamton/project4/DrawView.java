7036dcf09f71992f37d8cc442b5261e91f93676a
Frankie <email@frankiescott.net>
2019-07-08 10:07:08 -0400

    drew graph coordinate lines

--- app/src/main/java/edu/binghamton/project4/DrawView.java
+++ app/src/main/java/edu/binghamton/project4/DrawView.java
@@ -8,7 +8,10 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class DrawView extends View {
-    Paint paint = new Paint();
+    Paint line_paint;
+    Paint axis_paint;
+    Paint tick_paint;
+    Paint coord_paint;
 
     int maxX;
     int maxY;
@@ -17,13 +20,37 @@ public class DrawView extends View {
     int m;
     int b;
 
+    public void init() {
+        line_paint = new Paint();
+        axis_paint = new Paint();
+        tick_paint = new Paint();
+        coord_paint = new Paint();
+
+        line_paint.setColor(Color.BLACK);
+        line_paint.setStrokeWidth(3F);
+
+        axis_paint.setColor(Color.BLACK);
+        axis_paint.setStrokeWidth(4F);
+
+        tick_paint.setColor(Color.BLACK);
+        tick_paint.setStrokeWidth(2F);
+
+        coord_paint.setColor(Color.LTGRAY);
+        coord_paint.setStrokeWidth(1F);
+
+    }
     //override both constructors
-    public DrawView(Context c) { super(c); }
+    public DrawView(Context c) {
+        super(c);
+        init();
+    }
     public DrawView(Context c, AttributeSet s) {
         super(c, s);
+        init();
     }
     public DrawView(Context c, AttributeSet s, int defStyle) {
         super(c, s, defStyle);
+        init();
     }
 
     @Override
@@ -33,14 +60,12 @@ public class DrawView extends View {
         maxX = canvas.getWidth();
         maxY = canvas.getHeight();
 
-        setBackgroundColor(Color.LTGRAY);
-        paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(3F);
+        setBackgroundColor(Color.WHITE);
 
         //y axis
-        canvas.drawLine(maxX / 2, maxY, maxX / 2, 0, paint);
+        canvas.drawLine(maxX / 2, maxY, maxX / 2, 0, axis_paint);
         //x axis
-        canvas.drawLine(0,maxY / 2, maxX, maxY / 2, paint);
+        canvas.drawLine(0,maxY / 2, maxX, maxY / 2, axis_paint);
 
         int width_x = maxX / 12; //dividing coordinate plane by 12
         int width_y = maxY / 12;
@@ -48,17 +73,19 @@ public class DrawView extends View {
         //x axis coordinates
         int start_x = 0;
         for (int i = 0; i <= maxX; ++i) {
-            canvas.drawLine(start_x,(maxY / 2) - 10, start_x, (maxY / 2) + 10, paint);
+            canvas.drawLine(start_x,(maxY / 2) - 10, start_x, (maxY / 2) + 10, tick_paint);
+            canvas.drawLine(start_x,0, start_x, maxY, coord_paint);
             start_x += width_x;
         }
         //y axis coordinates
         int start_y = 0;
         for (int i = 0; i <= maxY; ++i) {
-            canvas.drawLine((maxX / 2) - 10,start_y, (maxX / 2) + 10, start_y, paint);
+            canvas.drawLine((maxX / 2) - 10,start_y, (maxX / 2) + 10, start_y, tick_paint);
+            canvas.drawLine(0,start_y, maxX, start_y, coord_paint);
             start_y += width_y;
         }
         //line
-        canvas.drawLine(0, maxY, 200, 200, paint);
+        canvas.drawLine(0, maxY, 200, 200, line_paint);
     }
 
     @Override
