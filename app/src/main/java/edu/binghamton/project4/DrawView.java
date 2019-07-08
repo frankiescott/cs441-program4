ad05f1a3a33148d2677f3422a56f85144052d5b3
Frankie <email@frankiescott.net>
2019-07-08 09:58:18 -0400

    drew x and y axis on the canvas

--- app/src/main/java/edu/binghamton/project4/DrawView.java
+++ app/src/main/java/edu/binghamton/project4/DrawView.java
@@ -5,11 +5,17 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.view.Display;
 import android.view.View;
 
 public class DrawView extends View {
     Paint paint = new Paint();
+
+    int maxX;
+    int maxY;
+    int x;
+    int y;
+    int m;
+    int b;
 
     //override both constructors
     public DrawView(Context c) { super(c); }
@@ -24,12 +30,34 @@ public class DrawView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        int maxX = canvas.getWidth();
-        int maxY = canvas.getHeight();
+        maxX = canvas.getWidth();
+        maxY = canvas.getHeight();
 
         setBackgroundColor(Color.LTGRAY);
         paint.setColor(Color.BLACK);
         paint.setStrokeWidth(3F);
+
+        //y axis
+        canvas.drawLine(maxX / 2, maxY, maxX / 2, 0, paint);
+        //x axis
+        canvas.drawLine(0,maxY / 2, maxX, maxY / 2, paint);
+
+        int width_x = maxX / 12; //dividing coordinate plane by 12
+        int width_y = maxY / 12;
+
+        //x axis coordinates
+        int start_x = 0;
+        for (int i = 0; i <= maxX; ++i) {
+            canvas.drawLine(start_x,(maxY / 2) - 10, start_x, (maxY / 2) + 10, paint);
+            start_x += width_x;
+        }
+        //y axis coordinates
+        int start_y = 0;
+        for (int i = 0; i <= maxY; ++i) {
+            canvas.drawLine((maxX / 2) - 10,start_y, (maxX / 2) + 10, start_y, paint);
+            start_y += width_y;
+        }
+        //line
         canvas.drawLine(0, maxY, 200, 200, paint);
     }
 
@@ -46,5 +74,9 @@ public class DrawView extends View {
             size = width;
         }
         setMeasuredDimension(size, size);
+    }
+
+    public int calculateY(int m, int b) {
+        return (m * 2) + b;
     }
 }
